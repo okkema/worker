271044bfff10b100da685479bf4c5a13e9abde69
Benjamin Okkema <ben@okkema.org>
2021-06-26 22:53:44 -0500

    Add common responses

--- src/core/__test__/responses.test.ts
+++ src/core/__test__/responses.test.ts
@@ -1,9 +1,47 @@
-import { Ok } from "../responses"
+import {
+  BadRequest,
+  Forbidden,
+  InternalServerError,
+  NoContent,
+  NotFound,
+  Ok,
+  Unauthorized,
+} from "../responses"
 
 describe("responses", () => {
-  it("should return an Ok", () => {
+  it("should return an 200 OK", () => {
     const result = Ok()
     expect(result.status).toBe(200)
-    expect(result.statusText).toBe("Ok")
+    expect(result.statusText).toBe("OK")
+  })
+  it("should return a 204 No Content", () => {
+    const result = NoContent()
+    expect(result.status).toBe(204)
+    expect(result.statusText).toBe("No Content")
+  })
+  it("should return a 400 Bad Request", () => {
+    const result = BadRequest()
+    expect(result.status).toBe(400)
+    expect(result.statusText).toBe("Bad Request")
+  })
+  it("should return a 401 Unauthorized", () => {
+    const result = Unauthorized()
+    expect(result.status).toBe(401)
+    expect(result.statusText).toBe("Unauthorized")
+  })
+  it("should return a 403 Forbidden", () => {
+    const result = Forbidden()
+    expect(result.status).toBe(403)
+    expect(result.statusText).toBe("Forbidden")
+  })
+  it("should return a 404 Not Found", () => {
+    const result = NotFound()
+    expect(result.status).toBe(404)
+    expect(result.statusText).toBe("Not Found")
+  })
+  it("should return a 500 Internal Server Error", () => {
+    const result = InternalServerError()
+    expect(result.status).toBe(500)
+    expect(result.statusText).toBe("Internal Server Error")
   })
 })
--- src/core/responses.ts
+++ src/core/responses.ts
@@ -1,2 +1,26 @@
-export const Ok = (body?: BodyInit): Response =>
-  new Response(body, { status: 200, statusText: "Ok" })
+const createResponse = (
+  status: number,
+  statusText: string,
+  body?: BodyInit,
+): Response => new Response(body, { status, statusText })
+
+export const Ok = (body: BodyInit = "OK"): Response =>
+  createResponse(200, "OK", body)
+
+export const NoContent = (): Response => createResponse(204, "No Content")
+
+export const BadRequest = (body: BodyInit = "Bad Request"): Response =>
+  createResponse(400, "Bad Request", body)
+
+export const Unauthorized = (body: BodyInit = "Unauthorized"): Response =>
+  createResponse(401, "Unauthorized", body)
+
+export const Forbidden = (body: BodyInit = "Forbidden"): Response =>
+  createResponse(403, "Forbidden", body)
+
+export const NotFound = (body: BodyInit = "Not Found"): Response =>
+  createResponse(404, "Not Found", body)
+
+export const InternalServerError = (
+  body: BodyInit = "Internal Server Error",
+): Response => createResponse(500, "Internal Server Error", body)
