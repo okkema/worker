60f241ab976f249f67ae3ea24f7a3253a550bc55
Benjamin Okkema <ben@okkema.org>
2021-07-07 18:57:43 -0500

    Update metadata

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,6 @@ module.exports = {
         test: /\.tsx?$/,
         loader: "ts-loader",
         options: {
-          // transpileOnly is useful to skip typescript checks occasionally:
-          // transpileOnly: true,
         },
       },
     ],
