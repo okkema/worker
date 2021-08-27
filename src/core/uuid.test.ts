6a2f8952b2d0c1fdc0895f1a8c0d6875753fe596
Benjamin Okkema <ben@okkema.org>
2021-08-26 20:05:34 -0500

    Move and refactor uuid tests

---
+++ src/core/uuid.test.ts
@@ -0,0 +1,18 @@
+import { uuid } from "./uuid"
+
+describe("uuid", () => {
+  it("generates a uuid in correct format", () => {
+    const regex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    const result = uuid()
+    expect(regex.test(result)).toBe(true)
+  })
+  it("not generate the same uuid", () => {
+    const test = []
+    for (let i = 0; i < 10000; i++) {
+      test.push(uuid())
+    }
+    const result = uuid()
+    expect(test.includes(result)).toBe(false)
+  })
+})
