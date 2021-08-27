f12a09adcda1ba752bbb3dca3e5ea28ec61c41c3
Benjamin Okkema <ben@okkema.org>
2021-08-27 10:29:28 -0500

    Remove example files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { handler } from "./routes"
-import Worker from "./core/worker"
-import SentryLogger from "./sentry/logger"
-
-declare const DSN: string
-
-Worker({ handler: handler, logger: SentryLogger({ DSN }) })
