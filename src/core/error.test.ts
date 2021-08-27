48522d35cf5122088b93a1ddd76df9dadb60cf7f
Benjamin Okkema <ben@okkema.org>
2021-08-26 22:06:52 -0500

    Add new error test

--- src/core/error.test.ts
+++ src/core/error.test.ts
@@ -11,7 +11,7 @@ describe("CoreError", () => {
     expect(result.detail).toBe(CoreError.name)
   })
 
-  it("assigns the init values", () => {
+  it("sets the init values", () => {
     const init = {
       detail: "detail",
       status: 200,
@@ -25,5 +25,15 @@ describe("CoreError", () => {
     expect(result.title).toBe(init.title)
     expect(result.status).toBe(init.status)
     expect(result.detail).toBe(init.detail)
+  })
+  it("can be extended", () => {
+    class TestError extends CoreError {}
+    const result = new TestError()
+    expect(result.name).toBe(TestError.name)
+    expect(result.message).toBe(TestError.name)
+    expect(result.type).toBe(TestError.name)
+    expect(result.title).toBe(TestError.name)
+    expect(result.status).toBe(500)
+    expect(result.detail).toBe(TestError.name)
   })
 })
