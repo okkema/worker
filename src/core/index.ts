00411064dd48359c6ace264adc85e7255db41f3c
Benjamin Okkema <ben@okkema.org>
2021-11-27 23:10:39 -0600

    Update readme

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,6 +5,7 @@ import Worker, {
 } from "./worker"
 import Router, { RequestHandler, Route } from "./router"
 import Problem from "./problem"
+import CORS from "./cors"
 export { Worker, Router, Problem }
 export type {
   FetchEventHandler,
@@ -12,5 +13,6 @@ export type {
   Logger,
   RequestHandler,
   Route,
+  CORS,
 }
 export default Worker
