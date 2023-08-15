9c664d0b5ae9b79f2de2f029a4dbe5f9786ceb71
Benjamin Okkema <ben@okkema.org>
2023-08-14 23:42:15 -0500

    docs: update example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,14 +1,11 @@
-import { Problem } from "@okkema/worker"
+import { Problem, Worker } from "@okkema/worker"
+import type { Environment } from "@okkema/worker"
 
-type Environment = {
-  BUCKET: R2Bucket
-}
-
-export default {
-  async fetch(request: Request, _: Environment) {
+export default Worker({
+  async fetch(request: Request, _?: Environment) {
     const url = new URL(request.url)
     if (url.pathname === "/error")
       throw new Problem({ detail: "detail", title: "title" })
     return new Response("success")
   },
-}
+})
