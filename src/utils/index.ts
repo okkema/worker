7b776080d623394daa949c302eaa37685d36d6c6
Benjamin Okkema <ben@okkema.org>
2023-08-16 20:20:28 -0500

    feat: remove randomize

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from "./base64"
 export * from "./sanitze"
-export * from "./randomize"
--- src/utils/randomize.ts
+++
@@ -1,3 +0,0 @@
-export function randomize(length: number): Uint8Array {
-  return crypto.getRandomValues(new Uint8Array(length))
-}
