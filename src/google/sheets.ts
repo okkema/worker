4c1aac387deadcfb5ec903c2585b9ae4c104cf4a
Benjamin Okkema <ben@okkema.org>
2025-03-20 21:39:22 -0500

    feat: add sheets update

--- src/google/sheets.ts
+++ src/google/sheets.ts
@@ -1,11 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { KVNamespace } from "@cloudflare/workers-types"
 import { Oauth } from "./oauth"
 
 const scope = "https://www.googleapis.com/auth/spreadsheets.readonly"
 
+export type GoogleSheetsRow = {
+  $range: string
+  $order: string
+}
+
 type GoogleSheets = {
   get(spreadsheetId: string, range: string): Promise<any[][]>
-  parse<T>(spreadsheetId: string, range: string): Promise<T[]>
+  parse<T extends GoogleSheetsRow>(
+    spreadsheetId: string,
+    range: string,
+  ): Promise<T[]>
+  update<T extends GoogleSheetsRow>(
+    spreadsheetId: string,
+    range: string,
+    ...values: T[]
+  ): Promise<boolean>
 }
 
 type GetResponse = {
@@ -14,8 +28,28 @@ type GetResponse = {
   values: any[]
 }
 
-export function GoogleSheets(credentials: string): GoogleSheets {
-  const oauth = Oauth(credentials)
+type UpdateResponse = {
+  spreadsheetId: string
+  totalUpdatedRows: number
+  totalUpdatedColumns: number
+  totalUpdatedCells: number
+  totalUpdatedSheets: number
+  responses: any[]
+}
+
+function getRange(range: string, index: number) {
+  const parts = range.split(":")
+  const row = parts[0].match(/\d+/)
+  const startRow = row != null ? +row[0] : 0
+  const startCol = parts[0].replace(/^\d+/g, "")
+  return `${startCol}${startRow + index}`
+}
+
+export function GoogleSheets(
+  credentials: string,
+  kv?: KVNamespace,
+): GoogleSheets {
+  const oauth = Oauth(credentials, kv)
   async function get(spreadsheetId: string, range: string) {
     const token = await oauth.token(scope)
     const response = await fetch(
@@ -27,19 +61,48 @@ export function GoogleSheets(credentials: string): GoogleSheets {
   }
   return {
     get,
-    async parse<T>(spreadsheetId, range) {
+    async parse<T extends GoogleSheetsRow>(spreadsheetId, range) {
       const rows = await get(spreadsheetId, range)
       const keys = rows.shift()
       const values = []
-      for (const row of rows) {
-        const value = {}
-        for (let i = 0; i < row.length; i++) {
-          const key = keys[i]
-          value[key] = row[i]
+      for (let i = 0; i < rows.length; i++) {
+        const value = { $range: getRange(range, i) }
+        const order = []
+        for (let j = 0; j < rows[i].length; j++) {
+          const key = keys[j]
+          value[key] = rows[i][j]
+          order.push(key)
         }
+        value["$order"] = order.join("|")
         values.push(value)
       }
       return values satisfies T[]
+    },
+    async update(spreadsheetId, range, ...values) {
+      const token = await oauth.token(scope)
+      const response = await fetch(
+        `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values:batchUpdate`,
+        {
+          method: "POST",
+          headers: { Authorization: `Bearer ${token}` },
+          body: JSON.stringify({
+            data: values.map(function (value) {
+              const order = value.$order.split("|")
+              const result = []
+              for (const key of order) {
+                result.push(value[key])
+              }
+              return {
+                range: value.$range,
+                majorDimension: "ROWS",
+                values: result,
+              }
+            }),
+          }),
+        },
+      )
+      const body = await response.json<UpdateResponse>()
+      return body.totalUpdatedRows == values.length
     },
   }
 }
