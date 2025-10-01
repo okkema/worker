15b02097d68d5b904712b6dd25837e890cb686d0
Benjamin Okkema <ben@okkema.org>
2025-09-30 20:37:24 -0500

    fix: logging attributes

--- src/api/error.ts
+++ src/api/error.ts
@@ -6,8 +6,8 @@ export async function error(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   c: Context,
 ) {
-  console.error("Unhandled error %s", error.name)
-  if (error.cause) console.error("Root cause: %s", error.cause)
+  console.error("Unhandled error ", error.name)
+  if (error.cause) console.error("Root cause ", error.cause)
   console.error(error.message)
   if (error.stack) console.error(error.stack)
   if (error instanceof Problem) return error.response
